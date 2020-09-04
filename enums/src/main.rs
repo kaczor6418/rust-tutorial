79cc4b88ebb908435511296b5e45d236eec0704e
kaczor6418 <k.kaczynski97@gmail.com>
2020-09-04 23:45:15 +0200

    feat(enums) how to create enums and enums kinds

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,11 @@
+#[derive(Debug)]
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let ip_v4 = IpAddr::V4(127, 0, 0, 1);
+    let ip_v6 = IpAddr::V6(String::from("::1"));
+    println!("{:#?}", ip_v4);
 }
