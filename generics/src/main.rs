d40508fb992d91240cd2593da0dcaf5d2350a1ac
kaczor6418 <k.kaczynski97@gmail.com>
2020-11-10 22:17:24 +0100

    wip(generics): learn some about generics

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -3,22 +3,22 @@ struct Point<T> {
     y: T,
 }
 
-impl<T> Point<T> {
-    fn new(x: T, y: T) -> Point<T> {
-        return Point {
-            x,
-            y,
-        };
-    }
-
-    pub fn x_difference(&self, target_point: Point<T>) -> T {
-        return (self.x - target_point.x).abs();
-    }
-
-    pub fn y_difference(&self, target_point: Point<T>) -> T {
-        return (self.y - target_point.y).abs();
-    }
-}
+// impl<T> Point<T> {
+//     fn new(x: T, y: T) -> Point<T> {
+//         return Point {
+//             x,
+//             y,
+//         };
+//     }
+//
+//     pub fn x_difference(&self, target_point: Point<T>) -> T {
+//         return (self.x - target_point.x).abs();
+//     }
+//
+//     pub fn y_difference(&self, target_point: Point<T>) -> T {
+//         return (self.y - target_point.y).abs();
+//     }
+// }
 
 fn main() {
     //traits
@@ -31,7 +31,7 @@ fn main() {
     let floating_point1: Point<f32> = Point::new(12.32, 54.11); // Coordinates of this point can be only floats values
     let floating_point2: Point<f32> = Point::new(14.32, -14.11);
     println!("X difference between int points: {:?}", int_point1.x_difference(int_point2));
-    println!("Y difference between int points: {:?}", int_point1.y_difference(int_point2));
-    println!("X difference between floating points: {:?}", floating_point1.x_difference(floating_point2));
-    println!("Y difference between floating points: {:?}", floating_point1.y_difference(floating_point2));
+    // println!("Y difference between int points: {:?}", int_point1.y_difference(int_point2));
+    // println!("X difference between floating points: {:?}", floating_point1.x_difference(floating_point2));
+    // println!("Y difference between floating points: {:?}", floating_point1.y_difference(floating_point2));
 }
