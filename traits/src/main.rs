5eb67ace986fbfd5b33b2e9d6b94da3260d1689d
kaczor6418 <k.kaczynski97@gmail.com>
2020-11-13 00:46:12 +0100

    feat(traits): add example with traits

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -13,6 +13,12 @@ struct Triangle {
     c: f64,
 }
 
+struct Cube {
+    a: f64,
+    b: f64,
+    c: f64,
+}
+
 struct Person {
     name: String,
     age: i32,
@@ -23,6 +29,9 @@ In traits we can declare only a signature of method (without implementation)
 Differences:
   - can not declare attributes
   - in shape implementation you can not declare other method. This implementation can contains only **`trait members`**
+  - `+` is kind of intersection of two or more Traits it means that object has to be both or more traits in the same time
+  - `where` to make code more clean if **Intersection** typeis long and use in more generics we can use where after method declaration
+  - if return type of method is a `impl Trait` then type which implements this trait want be allowed as return type
 */
 
 trait Shape {
@@ -31,6 +40,10 @@ trait Shape {
     }
     fn circuit(&self) -> f64;
     fn area(&self) -> f64;
+}
+
+trait Shape3D {
+    fn volume(&self) -> f64;
 }
 
 impl Shape for Square {
@@ -64,8 +77,39 @@ impl Shape for Triangle {
     }
 }
 
+impl Shape for Cube {
+    fn common_method(&self) -> String {
+        return String::from("I am a 3D Shape");
+    }
+
+    fn circuit(&self) -> f64 {
+        return self.a * 4.0 + self.b * 4.0 + self.c * 4.0;
+    }
+
+    fn area(&self) -> f64 {
+        return self.a * self.b * 2.0 + self.a * self.c * 2.0 + self.b * self.c * 2.0;
+    }
+}
+
+impl Shape3D for Cube {
+    fn volume(&self) -> f64 {
+        return self.a * self.b * self.c;
+    }
+}
+
 fn accepts_only_shapes(shape: &impl Shape) {
     println!("{}", shape.common_method())
+}
+
+fn accepts_only_3d_shapes<T: Shape + Shape3D>(shape_3d: &T) {
+    println!("{}", shape_3d.common_method())
+}
+
+fn accepts_only_3d_shapes_with_where<T>(shape_3d: &T)
+where
+    T: Shape + Shape3D,
+{
+    println!("{}", shape_3d.common_method())
 }
 
 // trait bound syntax
@@ -78,14 +122,15 @@ fn main() {
         b: 4.0,
         c: 5.0,
     }; // circuit: 12, area: 6
+    let cube = Cube {
+        a: 2.0,
+        b: 3.0,
+        c: 4.0,
+    }; // circuit: 36, area: 52, volume: 24
     let person = Person {
         name: String::from("Jan"),
         age: 32,
     };
-    accepts_only_shapes(&square);
-    accepts_only_shapes(&rectangle);
-    accepts_only_shapes(&triangle);
-    // accepts_only_shapes(&person); will trow this error: the trait `Shape` is not implemented for `Person`
     println!(
         "Square → Circuit: {}, Area: {}",
         square.circuit(),
@@ -101,4 +146,17 @@ fn main() {
         triangle.circuit(),
         triangle.area()
     );
+    println!(
+        "Cube → Circuit: {}, Area: {}, Volume: {}",
+        cube.circuit(),
+        cube.area(),
+        cube.volume()
+    );
+    accepts_only_shapes(&square);
+    accepts_only_shapes(&rectangle);
+    accepts_only_shapes(&triangle);
+    // accepts_only_shapes(&person); will trow this error: the trait `Shape` is not implemented for `Person`
+    accepts_only_3d_shapes(&cube);
+    accepts_only_3d_shapes_with_where(&cube);
+    // accepts_only_3d_shapes(&triangle); will throw this error: the trait `Shape3D` is not implemented for `Triangle`
 }
