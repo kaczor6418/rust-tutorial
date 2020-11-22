190e250be187640c960b8453133fd85e4e1e9a89
kaczor6418 <k.kaczynski97@gmail.com>
2020-11-22 18:57:53 +0100

    feat(test): use result to show more custom asserts

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -10,17 +10,25 @@ mod rectangle_test {
     /// this macro will let Rust know that this test should be executed if cargo test
     /// `assert!` macro test is code executed inside return true
     #[test]
-    fn rectangle_with_smaller_circuit_should_fit_in_rectangle_with_bigger_circuit() {
-        let bigger_rectangle = Rectangle::new(2.00, 3.00);
-        let smaller_rectangle = Rectangle::new(1.00, 2.00);
-        assert!(bigger_rectangle.can_hold(&smaller_rectangle));
-    }
-
-    #[test]
     fn rectangle_with_smaller_circuit_should_not_fit_in_rectangle_with_bigger_circuit() {
         let smaller_rectangle = Rectangle::new(1.00, 2.00);
         let bigger_rectangle = Rectangle::new(2.00, 3.00);
         assert!(!smaller_rectangle.can_hold(&bigger_rectangle));
+    }
+
+    /// If we need more custom assert, then we can use `Result` enum to tell test when test result is ok and when it is not
+    #[test]
+    fn rectangle_with_smaller_circuit_should_fit_in_rectangle_with_bigger_circuit(
+    ) -> Result<(), String> {
+        let bigger_rectangle = Rectangle::new(2.00, 3.00);
+        let smaller_rectangle = Rectangle::new(1.00, 2.00);
+        return if bigger_rectangle.can_hold(&smaller_rectangle) {
+            Ok(())
+        } else {
+            Err(String::from(
+                "Bigger rectangle couldn't hold smaller rectangle",
+            ))
+        };
     }
 
     #[test]
@@ -45,8 +53,9 @@ mod rectangle_test {
     }
 
     /// `should_panic` macro will let rust know that this test should throw an exception
+    /// If we wont precise panic message then it can be any panic message
     #[test]
-    #[should_panic]
+    #[should_panic = "I told you that i will panic!"]
     fn rectangle_should_panic_after_calling_will_panic() {
         let rectangle_1 = Rectangle::new(2.00, 3.00);
         rectangle_1.i_will_panic();
