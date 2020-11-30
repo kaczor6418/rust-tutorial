e0e98588482f290f1e4af4d614bcca0cdf6c5988
kaczor6418 <k.kaczynski97@gmail.com>
2020-12-01 00:14:08 +0100

    wip(gneric-map): try to fix generic hash map

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -3,35 +3,38 @@ use std::hash::Hash;
 use std::thread;
 use std::time::Duration;
 
-struct Cacher<'a, T, U: 'a>
+struct Cacher<T, U>
 where
-    T: Fn(U) -> &'a U,
+    T: Fn(U) -> U,
 {
     calculation: T,
     values: HashMap<U, U>,
 }
 
-impl<'a, T, U: 'a> Cacher<'a, T, U>
+impl<T, U> Cacher<T, U>
 where
-    T: Fn(U) -> &'a U,
+    T: Fn(U) -> U,
     U: Eq + Hash + Clone,
 {
-    fn new(calculation: T) -> Cacher<'a, T, U> {
+    fn new(calculation: T) -> Cacher<T, U> {
         Cacher {
             calculation,
             values: HashMap::new(),
         }
     }
 
-    fn value(&mut self, arg: U) -> &U {
+    fn value(&mut self, arg: U) -> U {
         let result = self.values.get(&arg);
         match result {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.values.insert(arg.clone(), v);
-                return v;
-            }
+            Some(v) => v.clone(),
+            None => self
+                .values
+                .insert(arg.clone(), (self.calculation)(arg.clone()))
+                .unwrap_or_else(|| {
+                    eprintln!("error occurred");
+                    return arg;
+                })
+                .clone(),
         }
     }
 }
@@ -40,9 +43,8 @@ fn generate_workout(intensity: u32, random_number: u32) {
     let mut expensive_result = Cacher::new(|num: u32| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
-        &num
+        num
     });
-
     if intensity < 25 {
         println!("Today, do {} pushups!", expensive_result.value(intensity));
         println!("Next, do {} situps!", expensive_result.value(intensity));
@@ -59,7 +61,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
 }
 
 fn main() {
-    let simulated_user_specified_value = 100;
+    let simulated_user_specified_value = 11;
     let simulated_random_number = 3;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
@@ -67,10 +69,10 @@ fn main() {
 
 #[test]
 fn call_with_different_values() {
-    let mut c = Cacher::new(|a: i32| &a);
+    let mut c = Cacher::new(|a: i32| a);
 
     let v1 = c.value(1);
     let v2 = c.value(2);
 
-    assert_eq!(v2, &2);
+    assert_eq!(v2, 2);
 }
