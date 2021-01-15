2af65c829d983b5a27d77ecaa8fdb68ce6ca76c0
kaczor6418 <k.kaczynski97@gmail.com>
2021-01-15 01:58:25 +0100

    feat(box): implement my own simple smart pointer

---
+++ pointers/src/main.rs
@@ -0,0 +1,26 @@
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
+fn main() {
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m); // is equal hello(&(*m)[..]); because fir we will take a value under m (String in our case) then we will take a slice of this data so str and we will pass it as a reference &
+}
