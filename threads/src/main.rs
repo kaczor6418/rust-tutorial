aeb1ac59dc966c01f1318957ef77145bbb378779
kaczor6418 <k.kaczynski97@gmail.com>
2021-02-07 22:41:36 +0100

    chore(threads): stream with multiple transmitters and one receiver

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,3 +1,4 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
@@ -14,6 +15,8 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    println!("\n");
     println!("-------------------- Pass data from main thread to spawned thread:");
     let main_thread_vector = vec![1, 2, 3];
 
@@ -27,8 +30,76 @@ fn main() {
     let handle = thread::spawn(move || {
         println!("Variable from main thread: {:?}", main_thread_vector);
     });
-
     // tThis will throw an error because we moved main thread variable to spawned thread scope so we can not access this data from main scope anymore
     // println!("{:?}", main_thread_vector);
     handle.join().unwrap();
+
+    println!("\n");
+    println!("---------------------------------- Message passing (Streams):");
+    let (transmitter, receiver) = mpsc::channel();
+    thread::spawn(move || {
+        let messages_to_send = vec![
+            String::from("first message"),
+            String::from("second message"),
+            String::from("third message"),
+        ];
+        for message in messages_to_send {
+            transmitter.send(message).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    // To receive single message we ca use .recv but this method will block main thread :/
+    // let received_message = receiver.recv().unwrap();
+
+    for received in receiver {
+        println!("Received message: {}", received);
+    }
+    println!("Main thread is not blocked");
+
+    println!("\n");
+    println!("---------------------------------- Multiple transmitters:");
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let values = vec![
+            String::from("# FIRST THREAD: 1"),
+            String::from("# FIRST THREAD: 2"),
+            String::from("# FIRST THREAD: 3"),
+            String::from("# FIRST THREAD: 4"),
+        ];
+        for val in values {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vales = vec![
+            String::from("@ SECOND THREAD: 1"),
+            String::from("@ SECOND THREAD: 2"),
+            String::from("@ SECOND THREAD: 3"),
+            String::from("@ SECOND THREAD: 4"),
+        ];
+        for val in vales {
+            tx.send(val).unwrap();
+        }
+    });
+
+    let message_listener = thread::spawn(move || {
+        for received in rx {
+            println!("Received message: {}", received);
+        }
+    });
+
+    let main_thread_messages = vec![
+        String::from("& MAIN THREAD: 1"),
+        String::from("& MAIN THREAD: 2"),
+        String::from("& MAIN THREAD: 3"),
+        String::from("& MAIN THREAD: 4"),
+    ];
+    for message in main_thread_messages {
+        println!("MAIN THREAD MESSAGE: {}", message);
+    }
+    message_listener.join().unwrap();
 }
